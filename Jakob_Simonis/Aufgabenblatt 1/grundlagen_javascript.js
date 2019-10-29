b467c9b769e2d68663d67537f648ca3cbeef5809
jakobsim <56256668+jakobsim@users.noreply.github.com>
2019-10-29 09:22:25 +0100

    .

--- Jakob_Simonis/Aufgabenblatt 1/grundlagen_javascript.js
+++ Jakob_Simonis/Aufgabenblatt 1/grundlagen_javascript.js
@@ -2,7 +2,6 @@
 const maxBerwertung = 5; //Konstante für maximale Bewertung
 var anzahlBewertung = 20; //Variable für Anzahl von Bewertungen
 var bewertung = 3; //Bewertung selbst
-let bewertungsListe = []; //Array von Bewertungen
 
 
 console.log(maxBerwertung);
@@ -74,5 +73,4 @@ function auswahl(){
 }
 //Switch für Menueauswahl
 
-//Test
 
