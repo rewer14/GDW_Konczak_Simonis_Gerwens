05818c7c61cac6c6d242792100fac2be38d65561
Max Gerwens <50670793+Max603@users.noreply.github.com>
2019-11-04 13:54:58 +0100

    Install Modul for Task 3

--- Max_Gerwens/Aufgabe 3/Aufgabe_3_grundlagen_javascript.js
+++ Max_Gerwens/Aufgabe 3/Aufgabe_3_grundlagen_javascript.js
@@ -1,10 +1,11 @@
-'use strict'
-const readline = require('readline')
+'use strict';
+const readline = require('readline');
 //readline
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
+
 
 
 
