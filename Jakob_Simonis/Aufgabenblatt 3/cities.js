f80406d633d46b6a4af5ff56fe9feb53813d8d89
jakobsim <56256668+jakobsim@users.noreply.github.com>
2019-11-05 09:20:03 +0100

    added cities.json

--- Jakob_Simonis/Aufgabenblatt 3/cities.js
+++ Jakob_Simonis/Aufgabenblatt 3/cities.js
@@ -9,9 +9,33 @@ module.exports = {
 };
 */
 
-var express = require ('express');
-const cities = newObject();
-var app = express();
-app.local.cities = require('./cities.json');
+function Stadt(name,einwohnerzahl,bundesland){
+    this.name=name;
+    this.einwohnerzahl=einwohnerzahl;
+    this.bundesland=bundesland;
+}
 
-console.log(cities.einwohnerzahl);
+let Berlin = new Stadt('Berlin', 3748148, 'Berlin');
+let Hamburg = new Stadt('Hamburg', 1891810, 'Hamburg')
+let Muenchen = new Stadt ('Muenchen', 1471508, 'Muenchen');
+let Koeln = new Stadt ('Koeln', 1085664, 'Nordrheinwestfalen');
+let FrankfurtAmMain = new Stadt ('Frankfurt am Main', 753056, 'Hessen');
+let Stuttgart = new Stadt ('Stuttgart', 634830, 'Baden Württemberg');
+let Duesseldorf = new Stadt ('Duesseldorf', 619294, 'Nordrheinwestfalen');
+let Leipzig = new Stadt ('Leipzig', 587857, 'Sachsen');
+let Dortmund=new Stadt('Dortmund',587010,'Nordrheinwestfalen');
+let Essen = new Stadt ('Essen', 583109, 'Nordrheinwestfalen');
+var cities = [Berlin, Hamburg, Muenchen, Koeln, FrankfurtAmMain, Stuttgart, Duesseldorf, Leipzig, Dortmund, Essen];
+const fs=require('fs');
+const jsonpath='cities.json';
+
+const pushtofile=function(){
+    fs.writeFileSync(jsonpath,JSON.stringify(cities,null,4),function(err){
+        if(err) throw err;
+        console.log('complete');
+        
+    });
+};
+
+pushtofile();
+
