be1895538a3b8c12d66324e90ac14f6ac7e1dd3e
rewer14 <56253146+rewer14@users.noreply.github.com>
2019-11-10 16:56:54 +0100

    updates übung 3

--- Marvin_Konczak/uebung3/aufgabe3.js
+++ Marvin_Konczak/uebung3/aufgabe3.js
@@ -1,1 +1,5 @@
-const JSONREADER = require("jasonreader.js");
+const JSONReader = require("./jasonreader.js");
+
+JSONReader.addCity({name: "Vertania City", einwohnerzahl: 10, bundesland: "Kanto"});
+JSONReader.getCity("Vertania City");
+JSONReader.deleteCity("Vertania City");
--- Marvin_Konczak/uebung3/jasonreader.js
+++ Marvin_Konczak/uebung3/jasonreader.js
@@ -2,6 +2,62 @@ const fs = require("fs");
 const readJSON = (path, callback) => {
   fs.readFile(path, "utf8", callback);
 };
+
+const pathToCityJson = './cities.json'
+
+const getCity = function( name ){
+  const NAME = name
+  console.log( "Get City" )
+  readJSON( pathToCityJson, function(err, result){
+    let cities = JSON.parse(result)
+    cities.forEach(city => {
+      if( city.name == NAME ) console.log(city)
+    });
+  });
+}
+
+const deleteCity = function( name ){
+  console.log( "Delete City" )
+  const NAME = name
+  let CITYINDEX;
+  readJSON( pathToCityJson, function(err, result){
+    let cities = JSON.parse(result)
+    cities.forEach((city, index) => {
+      if( city.name == NAME ){
+        CITYINDEX = index
+      }
+    });
+    console.log(cities);
+    cities.splice(CITYINDEX, 1)
+
+    fs.writeFile( pathToCityJson, JSON.stringify(cities), err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    })
+  });
+
+}
+
+const addCity = function( city ){
+  // TODO: Sortiert einfügen.
+  console.log( "Add City" )
+  readJSON( pathToCityJson, function(err, result){
+    let cities = JSON.parse(result)
+    cities.push( city );
+    fs.writeFile( pathToCityJson, JSON.stringify(cities), err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    })
+  });
+}
+
 module.exports = {
-  readJSON
+  readJSON,
+  getCity,
+  deleteCity,
+  addCity
 };
